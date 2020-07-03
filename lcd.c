7b245032b4a467e34e2c34c67556f8e87df11c5c
Richard Tomlinson <rpt@rpt.me.uk>
2020-07-03 16:18:06 +0100

    Removed 8 bit mode to save space

--- lcd.c
+++ lcd.c
@@ -7,14 +7,14 @@
  #include "lcd.h"
  #include "millis.h"
 
-static uint8_t _displayfunction;
+#define DISPLAY_FUNCTION  (LCD_4BITMODE | LCD_1LINE | LCD_5x8DOTS | LCD_2LINE)
+
 static uint8_t _numlines;
 static uint8_t _displaycontrol;
 static uint8_t _displaymode;
 static uint8_t _row_offsets[4];
 
 static void write4bits(uint8_t value);
-static void write8bits(uint8_t value);
 static void send(uint8_t value, uint8_t mode);
 static inline void command(uint8_t value);
 
@@ -39,30 +39,14 @@ void lcd_init()
     // Initialise the LCD interface e.g. I2C
     lcdIFInit();
 
-#ifdef EIGHT_BIT_MODE
-    _displayfunction = LCD_8BITMODE | LCD_1LINE | LCD_5x8DOTS;
-#else
-    _displayfunction = LCD_4BITMODE | LCD_1LINE | LCD_5x8DOTS;
-#endif
-    
     lcd_begin(16, 1, LCD_5x8DOTS);
 }
 
 void lcd_begin(uint8_t cols, uint8_t lines, uint8_t dotsize)
 {
-    if (lines > 1)
-    {
-        _displayfunction |= LCD_2LINE;
-    }
     _numlines = lines;
 
     lcd_setRowOffsets(0x00, 0x40, 0x00 + cols, 0x40 + cols);
-
-    // for some 1 line displays you can select a 10 pixel high font
-    if ((dotsize != LCD_5x8DOTS) && (lines == 1))
-    {
-        _displayfunction |= LCD_5x10DOTS;
-    }
 
     // SEE PAGE 45/46 FOR INITIALIZATION SPECIFICATION!
     // according to datasheet, we need at least 40ms after power rises above 2.7V
@@ -75,46 +59,23 @@ void lcd_begin(uint8_t cols, uint8_t lines, uint8_t dotsize)
     lcdRW( false );
     delayMicroseconds(1000000);
 
-    //put the LCD into 4 bit or 8 bit mode
-    if (! (_displayfunction & LCD_8BITMODE))
-    {
-        // this is according to the hitachi HD44780 datasheet
-        // figure 24, pg 46
+    //put the LCD into 4 bit mode
+    // this is according to the hitachi HD44780 datasheet
+    // page 45 figure 23
 
-        // we start in 8bit mode, try to set 4 bit mode
-        write4bits(0x03);
-        delayMicroseconds(4500); // wait min 4.1ms
+    // Send function set command sequence
+    command(LCD_FUNCTIONSET | DISPLAY_FUNCTION);
+    delayMicroseconds(4500);  // wait more than 4.1ms
 
-        // second try
-        write4bits(0x03);
-        delayMicroseconds(4500); // wait min 4.1ms
-        
-        // third go!
-        write4bits(0x03);
-        delayMicroseconds(150);
+    // second try
+    command(LCD_FUNCTIONSET | DISPLAY_FUNCTION);
+    delayMicroseconds(150);
 
-        // finally, set to 4-bit interface
-        write4bits(0x02);
-    }
-    else
-    {
-        // this is according to the hitachi HD44780 datasheet
-        // page 45 figure 23
-
-        // Send function set command sequence
-        command(LCD_FUNCTIONSET | _displayfunction);
-        delayMicroseconds(4500);  // wait more than 4.1ms
-
-        // second try
-        command(LCD_FUNCTIONSET | _displayfunction);
-        delayMicroseconds(150);
-
-        // third go
-        command(LCD_FUNCTIONSET | _displayfunction);
-    }
+    // third go
+    command(LCD_FUNCTIONSET | DISPLAY_FUNCTION);
 
     // finally, set # lines, font size, etc.
-    command(LCD_FUNCTIONSET | _displayfunction);
+    command(LCD_FUNCTIONSET | DISPLAY_FUNCTION);
 
     // turn the display on with no cursor or blinking default
     _displaycontrol = LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF;
@@ -283,32 +244,13 @@ static void write4bits( uint8_t value )
     lcd_pulseEnable();
 }
 
-static void write8bits(uint8_t value) 
-{
-#if 0
-    for (int i = 0; i < 8; i++)
-    {
-        digitalWrite(_data_pins[i], (value >> i) & 0x01);
-    }
-    
-    pulseEnable();
-#endif
-}
-
-// write either command or data, with automatic 4/8-bit selection
+// write either command or data 
 static void send(uint8_t value, uint8_t mode)
 {
     lcdRS( mode );
 
-    if (_displayfunction & LCD_8BITMODE)
-    {
-        write8bits(value);
-    }
-    else
-    {
-        write4bits(value>>4);
-        write4bits(value);
-    }
+    write4bits(value>>4);
+    write4bits(value);
 }
 
 void lcd_print( const char *string )
