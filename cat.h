ba5c5e737e45da9188c5da0aae27499b0ac247e9
Richard Tomlinson <rpt@rpt.me.uk>
2020-07-13 22:29:51 +0100

    Begun adding doxygen

--- cat.h
+++ cat.h
@@ -9,7 +9,10 @@
 #ifndef CAT_H_
 #define CAT_H_
 
+/// Initialise CAT control
 void catInit();
+
+/// Called from the main loop to do CAT control
 void catControl();
 
 #endif /* CAT_H_ */
--- i2c.h
+++ i2c.h
@@ -1,10 +1,31 @@
+/** \file cat.h
+ *
+ * \Author Richard Tomlinson G4TGJ
+ */ 
+
+
 #ifndef I2C_H
 #define I2C_H
 
 #include <inttypes.h>
 
+/// Initialise the I2C driver.
+///
+/// Must be called before any other I2C functions
 void i2cInit();
+
+/// Write to an 8 bit register over I2C.
+///
+/// @param[in] addr I2C address
+/// @param[in] reg Register address
+/// @param[in] data Data to write to register
 uint8_t i2cSendRegister(uint8_t addr, uint8_t reg, uint8_t data);
+
+/// Read from an 8 bit register over I2C.
+///
+/// @param[in] addr I2C address
+/// @param[in] reg Register address
+/// @param[out] data Pointer to data location to write contents of register to
 uint8_t i2cReadRegister(uint8_t addr, uint8_t reg, uint8_t *data);
 
 #endif //I2C_H
