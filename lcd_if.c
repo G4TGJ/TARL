386ff7512eaaa7312d3da9feaf4823dc0ed4efe4
Richard Tomlinson <rpt@rpt.me.uk>
2020-10-23 16:22:15 +0100

    Add lcd_if.c to select either port or I2C interface file

---
+++ lcd_if.c
@@ -0,0 +1,24 @@
+/*
+ * lcd_if.c
+ *
+ * Include either the LCD port interface or the I2C interface
+ *
+ * Created: 18/10/2020 13:47:03
+ * Author : Richard Tomlinson G4TGJ
+ */ 
+
+ #include "config.h"
+
+ #ifdef LCD_I2C
+
+ #include "lcd_i2c.c"
+
+ #elif LCD_PORT
+
+ #include "lcd_port.c"
+
+ #else
+
+ #error "Define either LCD_I2C or LCD_PORT"
+
+ #endif
